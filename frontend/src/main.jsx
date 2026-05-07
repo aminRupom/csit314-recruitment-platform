debece82217b3a6249680d237b70b2663e01a68d
Amin Rupom <aminulrupom2003@gmail.com>
2026-05-07 21:03:17 +1000

    Set basename for BrowserRouter in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/csit314-recruitment-platform">
       <App />
     </BrowserRouter>
   </React.StrictMode>
